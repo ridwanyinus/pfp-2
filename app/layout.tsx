1a71abb63ddfffae4f45e49edc21abc75b97dd72
Ridwan <ridwanyinusa15@gmail.com>
2024-07-15 22:50:24 +0100

    added changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from "./provider";
 export const metadata: Metadata = {
   title: "Ridwan Yinus - Front-End Developer & Graphic Designer in Nigeria | React, Next.js, Tailwind CSS",
   description:
-    "Explore Ridwan's portfolio showcasing a diverse range of front-end development projects and graphic design works. Specializing in React, Next.js, and Tailwind CSS, Ridwan delivers high-quality, visually appealing, and user-friendly websites. Discover innovative designs created with Adobe Photoshop and Adobe Illustrator, and learn more about Ridwan's skills and experience as a professional developer and designer from Nigeria.",
+    "Explore Ridwan's portfolio showcasing a diverse range of front-end development projects and graphic design works. Specializing in React, Next.js, and Tailwind CSS, Ridwan delivers high-quality, visually appealing, and user-friendly websites. Discover innovative designs created with Adobe-Photoshop and Adobe Illustrator, and learn more about Ridwan's skills and experience as a professional developer and designer from Nigeria.",
 };
 
 export default function RootLayout({
