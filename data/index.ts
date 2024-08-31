14a3e9383b44065575941c4a22e342723c3e6e80
Ridwan <ridwanyinusa15@gmail.com>
2024-08-31 16:13:24 +0100

    chore: refactor techstack

--- data/index.ts
+++ data/index.ts
@@ -1,20 +1,39 @@
+import html from '@/public/html-5.svg';
+import css from '@/public/css-3.svg';
+import js from '@/public/javascript.svg';
+import react from '@/public/react.svg';
+import typescript from '@/public/typescript.png';
+import tailwind from '@/public/tailwind.png';
+import ai from '@/public/illustrator.png';
+import github from '@/public/github-icon.svg';
+import git from '@/public/git-icon.svg';
+import canva from '@/public/canva.png';
+import figma from '@/public/figma.svg';
+import npm from '@/public/npm.svg';
+import vs from '@/public/visual-studio-code.svg';
+import varcel from '@/public/vercel-icon.svg';
+import netlify from '@/public/netlify.svg';
+import gitlab from '@/public/gitlab.svg';
+import ps from '@/public/photoshop.png';
+import nextjs from '@/public/next.svg';
+
 export const navItems = [
-  { name: "About", link: "#about" },
-  { name: "Services", link: "#service" },
-  { name: "Projects", link: "#projects" },
-  { name: "Contact", link: "#footer" },
+  { name: 'About', link: '#about' },
+  { name: 'Services', link: '#service' },
+  { name: 'Projects', link: '#projects' },
+  { name: 'Contact', link: '#footer' },
 ];
 
 export const gridItems = [
   {
     id: 2,
     title: "I'm very flexible with time zone communications",
-    description: "",
-    className: "sm:col-span-1",
-    imgClassName: "",
-    titleClassName: "justify-start",
-    img: "",
-    spareImg: "",
+    description: '',
+    className: 'sm:col-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-start',
+    img: '',
+    spareImg: '',
   },
 
   // {
@@ -29,94 +48,112 @@ export const gridItems = [
   // },
   {
     id: 6,
-    title: "Do you want to start a project together?",
-    description: "",
-    className: "sm:col-span-1",
-    imgClassName: "",
-    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-    img: "",
-    spareImg: "",
+    title: 'Do you want to start a project together?',
+    description: '',
+    className: 'sm:col-span-1',
+    imgClassName: '',
+    titleClassName: 'justify-center md:max-w-full max-w-60 text-center',
+    img: '',
+    spareImg: '',
   },
 ];
 
 export const projects = [
   {
     id: 1,
-    title: "Positivus: Navigating the digital landscape for success",
-    des: "Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.",
-    img: "/positivus.png",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/next.svg"],
-    link: "https://positivus-ryo.vercel.app/",
+    title: 'Positivus: Navigating the digital landscape for success',
+    des: 'Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media marketing, and content creation.',
+    img: '/positivus.png',
+    iconLists: ['/re.svg', '/tail.svg', '/ts.svg', '/next.svg'],
+    link: 'https://positivus-ryo.vercel.app/',
   },
   {
     id: 2,
-    title: "Brainwave: Your Ultimate Guide to Companion",
-    des: "With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.",
-    img: "/brainwave.png",
-    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg"],
-    link: "https://brainwave-ryo.vercel.app/",
+    title: 'Brainwave: Your Ultimate Guide to Companion',
+    des: 'With lots of unique blocks, you can easily build a page without coding. Build your next consultancy website within few minutes.',
+    img: '/brainwave.png',
+    iconLists: ['/next.svg', '/re.svg', '/tail.svg', '/ts.svg'],
+    link: 'https://brainwave-ryo.vercel.app/',
   },
   {
     id: 3,
-    title: "KNFT: Culinary Delights Await",
-    des: "Discover, buy, and sell unique KNFTs on our platform.",
-    img: "/knft.png",
-    iconLists: ["/re.svg", "/tail.svg", "/next.svg", "/ts.svg"],
-    link: "https://knft-ryo.vercel.app/",
+    title: 'KNFT: Culinary Delights Await',
+    des: 'Discover, buy, and sell unique KNFTs on our platform.',
+    img: '/knft.png',
+    iconLists: ['/re.svg', '/tail.svg', '/next.svg', '/ts.svg'],
+    link: 'https://knft-ryo.vercel.app/',
   },
 
   {
     id: 4,
-    title: "BankHotel: A Place of Tranquility and Community",
-    des: "Discover bankHotel, a serene hotel dedicated to nurturing community spirit and fostering connections.",
-    img: "/bank.jpg",
-    iconLists: ["/next.svg", "/re.svg", "/tail.svg", "/ts.svg"],
-    link: "https://bank-hotel-ryo.vercel.app",
+    title: 'BankHotel: A Place of Tranquility and Community',
+    des: 'Discover bankHotel, a serene hotel dedicated to nurturing community spirit and fostering connections.',
+    img: '/bank.jpg',
+    iconLists: ['/next.svg', '/re.svg', '/tail.svg', '/ts.svg'],
+    link: 'https://bank-hotel-ryo.vercel.app',
   },
 ];
 
 export const socialMedia = [
   {
     id: 1,
-    img: "/git.svg",
+    img: '/git.svg',
   },
   {
     id: 2,
-    img: "/twit.svg",
+    img: '/twit.svg',
   },
   {
     id: 3,
-    img: "/link.svg",
+    img: '/link.svg',
   },
 ];
 
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
-    className: "md:col-span-2",
-    thumbnail: "/exp1.svg",
+    title: 'Frontend Engineer Intern',
+    desc: 'Assisted in the development of a web-based platform using React.js, enhancing interactivity.',
+    className: 'md:col-span-2',
+    thumbnail: '/exp1.svg',
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
+    title: 'Mobile App Dev - JSM Tech',
+    desc: 'Designed and developed mobile app for both iOS & Android platforms using React Native.',
+    className: 'md:col-span-2', // change to md:col-span-2
+    thumbnail: '/exp2.svg',
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
+    title: 'Freelance App Dev Project',
+    desc: 'Led the dev of a mobile app for a client, from initial concept to deployment on app stores.',
+    className: 'md:col-span-2', // change to md:col-span-2
+    thumbnail: '/exp3.svg',
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
+    title: 'Lead Frontend Developer',
+    desc: 'Developed and maintained user-facing features using modern frontend technologies.',
+    className: 'md:col-span-2',
+    thumbnail: '/exp4.svg',
   },
+];
+
+export const techStack1 = [
+  { name: 'html', img: html },
+  { name: 'css', img: css },
+  { name: 'JAVASCRIPT', img: js },
+  { name: 'github', img: github },
+  { name: 'git', img: git },
+  { name: 'gitlab', img: gitlab },
+];
+
+export const techStack2 = [
+  { name: 'react', img: html },
+  { name: 'typescript', img: css },
+  { name: 'next.js', img: js },
+  { name: 'tailwind', img: github },
+  { name: 'vscode', img: git },
+  { name: 'npm', img: gitlab },
 ];
