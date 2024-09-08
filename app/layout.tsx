914ff3b18cdb1d1489b6e6476dd6a1975a6d932b
Ridwan <ridwanyinusa15@gmail.com>
2024-09-08 08:39:20 +0100

    chore: update desc

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { ThemeProvider } from "./provider";
-import Head from "next/head";
-import schemaData from "@/components/schema";
+import type { Metadata } from 'next';
+import './globals.css';
+import { ThemeProvider } from './provider';
+import Head from 'next/head';
+import schemaData from '@/components/schema';
 export const metadata: Metadata = {
-  title: "Ridwan Yinus - Front End Developer & Designer | React, Next.js",
-  description: "Explore Ridwan's portfolio of front-end projects and graphic designs. Specializing in React, Next.js, and Tailwind CSS for high-quality, user-friendly websites.",
+  title: 'Ridwan Yinus - Front End Developer & Designer | React, Next.js',
+  description: "Explore Ridwan's portfolio of front-end projects. Specializing in React, Next.js, and Tailwind CSS for high-quality, user-friendly websites.",
   verification: {
-    google: "IwEzWIUdKrFD6IMbBVaEowWRRvww402-YO9W9uLAZgc",
+    google: 'IwEzWIUdKrFD6IMbBVaEowWRRvww402-YO9W9uLAZgc',
   },
 };
 
@@ -17,33 +17,33 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <Head>
         <title>{String(metadata.title)}</title>
-        <meta name="description" content={String(metadata.description)} />
-        <meta name="google-site-verification" content="IwEzWIUdKrFD6IMbBVaEowWRRvww402-YO9W9uLAZgc" />
+        <meta name='description' content={String(metadata.description)} />
+        <meta name='google-site-verification' content='IwEzWIUdKrFD6IMbBVaEowWRRvww402-YO9W9uLAZgc' />
         {/* Open Graph Meta Tags */}
-        <meta property="og:title" content="Ridwan Yinus - Front End Developer & Graphic Designer" />
+        <meta property='og:title' content='Ridwan Yinus - Front End Developer & Graphic Designer' />
         <meta
-          property="og:description"
-          content="Explore the portfolio of Ridwan Yinus, a React front end developer and Graphic Designer specializing in creating high-quality, user-friendly websites using React, Next.js, and Tailwind CSS."
+          property='og:description'
+          content='Explore the portfolio of Ridwan Yinus, a React front end developer and Graphic Designer specializing in creating high-quality, user-friendly websites using React, Next.js, and Tailwind CSS.'
         />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://ridwanyinus.is-a.dev" />
-        <meta property="og:image" content="https://ridwanyinus.is-a.dev/hero.png" />
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://ridwanyinus.is-a.dev' />
+        <meta property='og:image' content='https://ridwanyinus.is-a.dev/hero.png' />
 
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Ridwan Yinus - Front End Developer & Graphic Designer" />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta name='twitter:title' content='Ridwan Yinus - Front End Developer & Graphic Designer' />
         <meta
-          name="twitter:description"
-          content="Explore the portfolio of Ridwan Yinus, a React front end developer and Graphic Designer specializing in creating high-quality, user-friendly websites using React, Next.js, and Tailwind CSS."
+          name='twitter:description'
+          content='Explore the portfolio of Ridwan Yinus, a React front end developer and Graphic Designer specializing in creating high-quality, user-friendly websites using React, Next.js, and Tailwind CSS.'
         />
-        <meta name="twitter:image" content="https://ridwanyinus.is-a.dev/hero.png" />
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }} />
+        <meta name='twitter:image' content='https://ridwanyinus.is-a.dev/hero.png' />
+        <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(schemaData) }} />
       </Head>
-      <body className="font-poppins">
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+      <body className='font-poppins'>
+        <ThemeProvider attribute='class' defaultTheme='dark' enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
