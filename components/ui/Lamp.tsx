71957600e5a4681831582699bd4a164d7dab4177
Ridwan <ridwanyinusa15@gmail.com>
2024-09-16 09:29:05 +0100

    chore: hidden for all 1536 above

--- components/ui/Lamp.tsx
+++ components/ui/Lamp.tsx
@@ -9,7 +9,7 @@ export function LampDemo() {
 
 export const LampContainer = ({ className }: { className?: string }) => {
   return (
-    <div className={cn('max-xl:hidden fourk:hidden absolute px-5 lg:pr-24  flex min-h-screen flex-col items-center justify-center overflow-hidden  w-full rounded-md z-0 mx-auto', className)}>
+    <div className={cn('max-xl:hidden 2xl:hidden absolute px-5 lg:pr-24  flex min-h-screen flex-col items-center justify-center overflow-hidden  w-full rounded-md z-0 mx-auto', className)}>
       <div className='absolute flex w-full  flex-1 scale-y-125 items-center justify-center isolate z-0 '>
         <motion.div
           initial={{ opacity: 0.5, width: '15rem' }}
