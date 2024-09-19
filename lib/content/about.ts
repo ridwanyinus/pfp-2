9fc7d4279ed0fb216eb4fbd584e060f350c7656e
Ridwan <ridwanyinusa15@gmail.com>
2024-09-19 21:15:26 +0100

    chore: remove nigerian origins

--- lib/content/about.ts
+++ lib/content/about.ts
@@ -2,7 +2,6 @@ export const about = [
   { name: 'Birthday', info: ':  Dec 05' },
   { name: 'Phone', info: ': +(234) 8166319747' },
   { name: 'Email', info: ': @ridwanyinusa15@gmail' },
-  { name: 'From', info: ': Lagos, Nigeria' },
   { name: 'Language', info: ': English | Yoruba | Hausa' },
   { name: 'Freelance', info: ': Available' },
 ];
