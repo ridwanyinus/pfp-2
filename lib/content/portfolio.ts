49d68180accc3d879c026065c6977ac8ccd0cb8b
Ridwan <ridwanyinusa15@gmail.com>
2024-10-14 17:19:54 +0100

    chore: update image cdn

--- lib/content/portfolio.ts
+++ lib/content/portfolio.ts
@@ -13,7 +13,7 @@ export const seoData = {
   title: 'Ridwan Yinus - Front End Developer & Designer | React, Next.js',
   description: "Explore Ridwan's portfolio of front-end projects. Specializing in React, Next.js, Typescript and Tailwind CSS for high-quality, user-friendly websites.",
   author: author.name,
-  image: 'https://cdn.discordapp.com/attachments/1284124195130966106/1284127909329502258/image.png?ex=66e580cf&is=66e42f4f&hm=457db5adc2c74f55477380c2dc58eda3e77219acb5c314d6c33f555c37d63387&',
+  image: 'https://tech-knight.sirv.com/ridwan.jpg',
   url: 'https://ridwanyinus.is-a.dev/',
   keywords: ['Ridwan', 'Ridwan Yinus', '@ridwanyinus', 'ridwanyinus', 'Portfolio', 'Ridwan Portfolio ', 'Ridwan Yinus Portfolio', 'Front end developer'],
 };
